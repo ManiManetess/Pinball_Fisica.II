b8b3fb096c09f964e3c1746602a47d5b4db1391c
ManiManetess <manelmontes03@gmail.com>
2024-10-23 14:08:59 +0200

    Map Include

---
+++ Source/ModuleGame.cpp
@@ -0,0 +1,292 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleRender.h"
+#include "ModuleGame.h"
+#include "ModuleAudio.h"
+#include "ModulePhysics.h"
+
+class PhysicEntity
+{
+protected:
+
+	PhysicEntity(PhysBody* _body, Module* _listener)
+		: body(_body)
+		, listener(_listener)
+	{
+		body->listener = listener;
+	}
+
+public:
+	virtual ~PhysicEntity() = default;
+	virtual void Update() = 0;
+
+	virtual int RayHit(vec2<int> ray, vec2<int> mouse, vec2<float>& normal)
+	{
+		return 0;
+	}
+
+protected:
+	PhysBody* body;
+	Module* listener;
+};
+
+class Circle : public PhysicEntity
+{
+public:
+	Circle(ModulePhysics* physics, int _x, int _y, Module* _listener, Texture2D _texture)
+		: PhysicEntity(physics->CreateCircle(_x, _y, 25), _listener)
+		, texture(_texture)
+	{
+
+	}
+
+	void Update() override
+	{
+		int x, y;
+		body->GetPhysicPosition(x, y);
+		Vector2 position{ (float)x, (float)y };
+		float scale = 1.0f;
+		Rectangle source = { 0.0f, 0.0f, (float)texture.width, (float)texture.height };
+		Rectangle dest = { position.x, position.y, (float)texture.width * scale, (float)texture.height * scale };
+		Vector2 origin = { (float)texture.width / 2.0f, (float)texture.height / 2.0f};
+		float rotation = body->GetRotation() * RAD2DEG;
+		DrawTexturePro(texture, source, dest, origin, rotation, WHITE);
+	}
+
+private:
+	Texture2D texture;
+
+};
+
+class Box : public PhysicEntity
+{
+public:
+	Box(ModulePhysics* physics, int _x, int _y, Module* _listener, Texture2D _texture)
+		: PhysicEntity(physics->CreateRectangle(_x, _y, 100, 50), _listener)
+		, texture(_texture)
+	{
+
+	}
+
+	void Update() override
+	{
+		int x, y;
+		body->GetPhysicPosition(x, y);
+		DrawTexturePro(texture, Rectangle{ 0, 0, (float)texture.width, (float)texture.height },
+			Rectangle{ (float)x, (float)y, (float)texture.width, (float)texture.height },
+			Vector2{ (float)texture.width / 2.0f, (float)texture.height / 2.0f}, body->GetRotation() * RAD2DEG, WHITE);
+	}
+
+	int RayHit(vec2<int> ray, vec2<int> mouse, vec2<float>& normal) override
+	{
+		return body->RayCast(ray.x, ray.y, mouse.x, mouse.y, normal.x, normal.y);;
+	}
+
+private:
+	Texture2D texture;
+
+};
+
+class Shape : public PhysicEntity
+{
+public:
+	// Pivot 0, 0
+
+	Shape(ModulePhysics* physics, int _x, int _y, int Points[], int num_points, Module* _listener, Texture2D _texture)
+		: PhysicEntity(physics->CreateChain(GetMouseX() - 50, GetMouseY() - 100, Points, num_points), _listener)
+		, texture(_texture)
+	{
+
+	}
+
+	void Update() override
+	{
+		int x, y;
+		body->GetPhysicPosition(x, y);
+		DrawTextureEx(texture, Vector2{ (float)x, (float)y }, body->GetRotation() * RAD2DEG, 1.0f, WHITE);
+	}
+
+private:
+	Texture2D texture;
+};
+
+
+
+ModuleGame::ModuleGame(Application* app, bool start_enabled) : Module(app, start_enabled)
+{
+	ray_on = false;
+	sensed = false;
+}
+
+ModuleGame::~ModuleGame()
+{}
+
+// Load assets
+bool ModuleGame::Start()
+{
+	LOG("Loading Intro assets");
+	bool ret = true;
+
+	App->renderer->camera.x = App->renderer->camera.y = 0;
+
+	circle = LoadTexture("Assets/wheel.png"); 
+	box = LoadTexture("Assets/crate.png");
+	rick = LoadTexture("Assets/rick_head.png");
+
+	Mapa_Pokemon = LoadTexture("Assets/Pokemon_Map.png");
+	
+	bonus_fx = App->audio->LoadFx("Assets/bonus.wav");
+
+	sensor = App->physics->CreateRectangleSensor(SCREEN_WIDTH / 2, SCREEN_HEIGHT, SCREEN_WIDTH, 50);
+
+	int Pokemon_Map[128] = {
+	193, 277,
+	192, 0,
+	1, 1,
+	0, 278,
+	56, 278,
+	21, 256,
+	10, 248,
+	7, 208,
+	12, 199,
+	24, 195,
+	24, 174,
+	28, 168,
+	9, 96,
+	12, 82,
+	12, 61,
+	16, 43,
+	22, 34,
+	30, 30,
+	49, 17,
+	65, 13,
+	79, 11,
+	96, 11,
+	111, 11,
+	123, 13,
+	134, 20,
+	141, 25,
+	150, 31,
+	157, 41,
+	162, 49,
+	171, 66,
+	172, 74,
+	175, 87,
+	177, 110,
+	178, 137,
+	174, 276,
+	161, 277,
+	161, 83,
+	157, 65,
+	148, 46,
+	130, 37,
+	111, 27,
+	98, 25,
+	78, 25,
+	66, 25,
+	50, 30,
+	44, 37,
+	41, 41,
+	87, 33,
+	105, 35,
+	125, 40,
+	135, 50,
+	141, 65,
+	150, 74,
+	150, 89,
+	148, 126,
+	140, 151,
+	132, 167,
+	137, 176,
+	137, 193,
+	150, 198,
+	154, 208,
+	154, 248,
+	138, 255,
+	106, 278
+	};
+
+	entities.emplace_back(new Shape(App->physics, 0, 0, Pokemon_Map, 128, this, Mapa_Pokemon));
+
+	return ret;
+}
+
+// Load assets
+bool ModuleGame::CleanUp()
+{
+	LOG("Unloading Intro scene");
+
+	return true;
+}
+
+// Update: draw background
+update_status ModuleGame::Update()
+{
+	if(IsKeyPressed(KEY_SPACE))
+	{
+		ray_on = !ray_on;
+		ray.x = GetMouseX();
+		ray.y = GetMouseY();
+	}
+
+	if(IsKeyPressed(KEY_ONE))
+	{
+		entities.emplace_back(new Circle(App->physics, GetMouseX(), GetMouseY(), this, circle));
+		
+	}
+
+	if(IsKeyPressed(KEY_TWO))
+	{
+		entities.emplace_back(new Box(App->physics, GetMouseX(), GetMouseY(), this, box));
+	}
+
+
+
+	// Prepare for raycast ------------------------------------------------------
+	
+	vec2i mouse;
+	mouse.x = GetMouseX();
+	mouse.y = GetMouseY();
+	int ray_hit = ray.DistanceTo(mouse);
+
+	vec2f normal(0.0f, 0.0f);
+
+	// All draw functions ------------------------------------------------------
+
+
+	for (PhysicEntity* entity : entities)
+	{
+		entity->Update();
+		if (ray_on)
+		{
+			int hit = entity->RayHit(ray, mouse, normal);
+			if (hit >= 0)
+			{
+				ray_hit = hit;
+			}
+		}
+	}
+	
+
+	// ray -----------------
+	if(ray_on == true)
+	{
+		vec2f destination((float)(mouse.x-ray.x), (float)(mouse.y-ray.y));
+		destination.Normalize();
+		destination *= (float)ray_hit;
+
+		DrawLine(ray.x, ray.y, (int)(ray.x + destination.x), (int)(ray.y + destination.y), RED);
+
+		if (normal.x != 0.0f)
+		{
+			DrawLine((int)(ray.x + destination.x), (int)(ray.y + destination.y), (int)(ray.x + destination.x + normal.x * 25.0f), (int)(ray.y + destination.y + normal.y * 25.0f), Color{ 100, 255, 100, 255 });
+		}
+	}
+
+	return UPDATE_CONTINUE;
+}
+
+void ModuleGame::OnCollision(PhysBody* bodyA, PhysBody* bodyB)
+{
+	App->audio->PlayFx(bonus_fx);
+}
